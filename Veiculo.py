a7386ee6cbbdb334912ca818a6c958ae3cf20581
Michele Lima <106484086+Mplimaa@users.noreply.github.com>
2025-09-09 11:28:37 -0300

    Update Veiculo.py

--- Veiculo.py
+++ Veiculo.py
@@ -1,5 +1,11 @@
+from abc import ABC, abstractmethod
+#abc = Abstract Base Classes (Módulo interno do Python).
+#ABC → transforma a classe em abstrata (não pode ser instanciada).
+# abstractmethod → marca métodos que devem ser implementados pelas classes filhas.
+
+
 # Classe mãe (superclasse)
-class Veiculo:
+class Veiculo(ABC):
     def __init__(self, marca, modelo):
         self.marca = marca
         self.modelo = modelo
@@ -7,6 +13,12 @@ class Veiculo:
     def acelerar(self):
         # Implementação genérica
         print("O veículo está acelerando de forma genérica.")
+    
+    #metodo abstrato obriga as classes filhas implementar, só pode ser feito nas filhas
+    #cls = a própria classe que chamou o método naquele momento  // pass instrucao que nao faz nada
+    @abstractmethod
+    def calcular_tempo_entrega(self,distancia):
+        pass
 
 
 
